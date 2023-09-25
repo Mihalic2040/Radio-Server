7f6588f645b42ab1610fc1a89690674b27646f1c
Mihalic2040 <mihabevza111@gmail.com>
2023-09-26 01:23:30 +0300

    G

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#screen -dmS update ./run_parser.sh
-./run_parser.sh
+screen -dmS update ./run_parser.sh
+#./run_parser.sh
 
-#uwsgi --socket 0.0.0.0:5000 --protocol=http -w app:app -p 10 --enable-threads
+uwsgi --socket 0.0.0.0:5000 --protocol=http -w app:app -p 10 --enable-threads
